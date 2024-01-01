c500300c6ccd439ff5a37aa8d255c590af2700ef
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-01-01 19:19:50 +0530

    custom api response and error handling

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
 import express from "express";
-console.log("hello world");
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
+const app = express();
+app.use(
+  cors({
+    origin: process.env.COROS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(
+  express.json({
+    limit: "20kb",
+  })
+);
+app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// const asyncHandler = () => {};
+// const asyncHandler = (func) => async() => {};
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     return res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+
+export { asyncHandler };
